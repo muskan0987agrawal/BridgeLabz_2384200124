6153c95ff0eea0c1608f5e6a219bf592b1b83f10
muskan0987agrawal <muskanaggarwalapril16@gmail.com>
2025-01-20 11:34:54 +0530

    [muskan][modify]modified the code

--- feature-19-01-25/AddNumber.cs
+++ feature-19-01-25/AddNumber.cs
@@ -6,8 +6,9 @@ class AddNumber
  {
    Console.Write("Enter the first number: ");
         int num1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter the second number: ");
+        Console.Write("Enter the second number: ");
         int num2 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("The sum is: " + (num1 + num2));
+		Console.ReadLine();
 		}
 }
