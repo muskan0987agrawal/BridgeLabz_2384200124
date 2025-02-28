af5db4e7931d0b87f9eac62cf71812c4468e874e
muskan0987agrawal <muskanaggarwalapril16@gmail.com>
2025-02-28 23:35:12 +0530

    [muskan]Added a ASP.Net Project1

---
+++ UserRegistrationAPI/BusinessLayer/UserRegistrationBL.cs
@@ -0,0 +1,21 @@
+﻿using RepositoryLayer;
+using Models;
+
+namespace BusinessLayer
+{
+    public class UserRegistrationBL
+    {
+        private readonly UserRegistrationRL _userRegistrationRL;
+
+        public UserRegistrationBL(UserRegistrationRL userRegistrationRL)
+        {
+            _userRegistrationRL = userRegistrationRL;
+        }
+
+        public string RegisterUser(UserModel user)
+        {
+            bool isRegistered = _userRegistrationRL.Register(user);
+            return isRegistered ? "Registration Successful" : "User Already Exists";
+        }
+    }
+}
---
+++ UserRegistrationAPI/UserRegistration/Controllers/UserRegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BusinessLayer;
+using Models;
+
+namespace UserRegistrationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserRegistrationController : ControllerBase
+    {
+        private readonly UserRegistrationBL _userRegistrationBL;
+
+        public UserRegistrationController(UserRegistrationBL userRegistrationBL)
+        {
+            _userRegistrationBL = userRegistrationBL;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserModel user)
+        {
+            var result = _userRegistrationBL.RegisterUser(user);
+            return Ok(new { message = result });
+        }
+    }
+}
